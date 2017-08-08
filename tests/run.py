d25e9105dcc7afa900760e272b5df1fcffbed43a
Borodin Gregory <grihabor@mail.ru>
2017-08-08 11:06:40 +0300

    move Command class to separate class

--- tests/run.py
+++ tests/run.py
@@ -1,45 +1,9 @@
 from collections import defaultdict
 import os
-import threading
-import subprocess
 import filecmp
 import platform
 import argparse
 import json
-
-
-TIME_LIMIT = 'TL'
-RUNTIME_ERROR = 'RE'
-OK = 'OK'
-WRONG_ANSWER = 'WA'
-
-
-class Command:
-    def __init__(self, args):
-        self.args = args
-        self.process = None
-
-    def run(self, timeout, cwd):
-        def target():
-            try:
-                self.process = subprocess.Popen(self.args, cwd=cwd)
-                self.process.communicate()
-            except OSError as e:
-                self.process.returncode = e
-
-        thread = threading.Thread(target=target)
-        thread.start()
-        thread.join(timeout)
-
-        if thread.is_alive():
-            self.process.kill()
-            thread.join()
-            return TIME_LIMIT
-
-        if self.process.returncode == 0:
-            return OK
-        else:
-            return RUNTIME_ERROR
 
 
 class Compressor:
