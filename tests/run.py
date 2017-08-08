8c9ecee01c014d759195c730701f9340154047fe
Borodin Gregory <grihabor@mail.ru>
2017-08-08 13:13:31 +0300

    use csv writer, refactor results

--- tests/run.py
+++ tests/run.py
@@ -1,12 +1,17 @@
+import csv
 from collections import defaultdict
 import os
-import filecmp
 import platform
 import argparse
 import json
 
-from tests.testing import OK, WRONG_ANSWER
 from tests.testing.compressor import Compressor
+
+METHOD = 'method'
+FILE = 'file'
+ORIGINAL_SIZE = 'original_size'
+COMPRESSED_SIZE = 'compressed_size'
+CONCLUSION = 'conclusion'
 
 
 def read_config(filename='config.cfg'):
@@ -24,7 +29,7 @@ def read_config(filename='config.cfg'):
 
 def run_test(method, exe_path, test_dir, test_file, timeout):
     test_path = os.path.join(test_dir, test_file)
-    size_before = os.path.getsize(test_path)
+    original_size = os.path.getsize(test_path)
 
     cmpr = Compressor(exe_path,
                       test_file,
@@ -34,38 +39,43 @@ def run_test(method, exe_path, test_dir, test_file, timeout):
 
     # Use context manager to clean up files
     with cmpr:
-        conclusion, size_after = cmpr.run_test()
+        conclusion, compressed_size = cmpr.run_test()
 
-    return {'file': test_file,
-            'size': size_before,
-            'compressed': size_after,
-            'conclusion': conclusion}
+    return {ORIGINAL_SIZE: original_size,
+            COMPRESSED_SIZE: compressed_size,
+            CONCLUSION: conclusion}
 
 
 def run_tests(methods, exe_path, test_dir, timeout=180.0):
-    res = defaultdict(list)
+    results = []
     for method in methods:
         for test_file in sorted(os.listdir(test_dir)):
             path = os.path.join(test_dir, test_file)
-            if os.path.isfile(path):
-                res[method].append(run_test(exe_path=exe_path,
-                                            test_dir=test_dir,
-                                            timeout=timeout,
-                                            test_file=test_file,
-                                            method=method))
-    return res
+            if not os.path.isfile(path):
+                continue
+
+            test_results = run_test(exe_path=exe_path,
+                                    test_dir=test_dir,
+                                    timeout=timeout,
+                                    test_file=test_file,
+                                    method=method)
+            test_results.update({METHOD: method,
+                                 FILE: test_file})
+            results.append(test_results)
+    return results
 
 
-def save_results(res, filename='res.csv'):
+def save_results(results, filename='res.csv'):
+    header = [METHOD,
+              FILE,
+              ORIGINAL_SIZE,
+              COMPRESSED_SIZE,
+              CONCLUSION]
     with open(filename, 'w') as resfile:
-        resfile.write('method,file,size,compressed,conclusion\n')
-        for key, value in res.items():
-            template = key + ',{},{},{},{}\n'
-            for sample in value:
-                resfile.write(template.format(sample['file'],
-                                              sample['size'],
-                                              sample['compressed'],
-                                              sample['conclusion']))
+        writer = csv.writer(resfile)
+        writer.writerow(header)
+        for row in results:
+            writer.writerow([row[column] for column in header])
 
 
 if __name__ == '__main__':
