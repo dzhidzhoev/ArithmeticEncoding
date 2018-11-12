df5ba723aa469de4ace911b61ad79b123902b2a2
Vladislav Dzhidzhoev <dzhidzhoev@gmail.com>
2018-11-12 23:22:23 +0300

    PPM realization.

--- src/ppm.c
+++ src/ppm.c
@@ -1,28 +1,223 @@
+#include <string.h>
+#include <math.h>
+#include <limits.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 
 #include "ppm.h"
+#include "utils.h"
+
+// #define DEBUG_PRINT
+// #define DEBUG_DATA
+
+static inline long long max(long long a, long long b) {
+    return a < b ? b : a;
+}
+
+static inline long long min(long long a, long long b) {
+    return a < b ? a : b;
+}
+
+static int prev_bits = 0;
+static void output_bit(FILE *out, struct bit_rw_buf *buf, unsigned val) {
+    write_bit(out, buf, val);
+    while (prev_bits) {
+        write_bit(out, buf, !val);
+        prev_bits--;
+    }
+}
+
+static unsigned read_bytes = 0;
+enum { N = 1 << CHAR_BIT };
+
+enum { FREQS_TABLE_DEPTH = 2 };
+
+typedef struct _freqs_table_entry {
+    unsigned freq;
+    int sym;
+    struct _freqs_table_entry *subtable;
+} freqs_table_entry;
+
+freqs_table_entry *create_freqs_table() {
+    freqs_table_entry *res = malloc(sizeof(*res) * N);
+    for (int i = 0; i < N; i++) {
+        res[i].freq = 1;
+        res[i].sym = i;
+        res[i].subtable = NULL;
+    }
+    return res;
+}
+
+static freqs_table_entry *global_table;
+static freqs_table_entry *freqs;
+static unsigned freq_ranges[N + 1];
+static int total_count;
+static int counts[N];
+static unsigned long long freqs_summ;
+static const unsigned long long MAX_FREQS_SUMM = (1 << 24) - 1;
+static const int WINDOW_LEN = 256;
+static const unsigned long long FREQ_ADD_COEFF = (1 << 8);
+static const int FREQS_SCALE_COEFF = 6;
+
+static int sym_history[FREQS_TABLE_DEPTH];
+
+typedef uint64_t value_type;
+value_type top_val;
+value_type half_val;
+value_type quart1_val;
+value_type quart3_val;
+
+static void clear_counts_table() {
+    read_bytes = 0;
+    for (int i = 0; i < N; i++) {
+        counts[i] = 0;
+    }
+}
+
+enum { BITS_USED = 40 };
+static void init_tables() {
+    top_val = (1ull << BITS_USED) - 1;
+    half_val = top_val / 2 + 1;
+    quart1_val = top_val / 4 + 1;
+    quart3_val = half_val + quart1_val;
+
+    for (int i = 0; i < N; i++) {
+        counts[i] = 0;
+    }
+
+    total_count = 0;
+
+    global_table = create_freqs_table();
+    freqs_summ = N;
+    freqs = global_table;
+
+    for (int i = 0; i < FREQS_TABLE_DEPTH; i++) {
+        sym_history[i] = 0;
+    }
+}
+
+static void modify_table(int ch) {
+    freqs[ch].freq += FREQ_ADD_COEFF;
+    global_table[ch].freq += FREQ_ADD_COEFF;
+    counts[ch]++;
+    ++read_bytes;
+
+    for (int i = 1; i < FREQS_TABLE_DEPTH; i++) {
+        sym_history[i - 1] = sym_history[i];
+    }
+    sym_history[FREQS_TABLE_DEPTH - 1] = ch;
+    freqs_table_entry *new_freqs = global_table;
+    for (int i = 0; i < FREQS_TABLE_DEPTH; i++) {
+        if (!new_freqs[sym_history[i]].subtable) {
+            new_freqs[sym_history[i]].subtable = create_freqs_table();
+        }
+        new_freqs = new_freqs[sym_history[i]].subtable;
+    }
+    freqs = new_freqs;
+}
+
+static void update_tables() {
+    freqs_summ = 0;
+    for (int i = 0; i < N; i++) {
+        freqs_summ += freqs[i].freq;
+    }
+    if (freqs_summ >= MAX_FREQS_SUMM) {
+        for (int i = 0; i < N; i++) {
+            if (freqs[i].freq) {
+                freqs[i].freq /= FREQS_SCALE_COEFF;
+                freqs[i].freq = max(freqs[i].freq, 1);
+            }
+        }
+        update_tables();
+        return;
+    }
+
+#ifdef DEBUG_PRINT
+    // printf("FREQS SUMM %d\n", freqs_summ);
+#endif
+}
+
+static void update_freqs_ranges() {
+    freq_ranges[0] = 0;
+    for (int i = 1; i <= N; i++) {
+        freq_ranges[i] = freq_ranges[i - 1] + freqs[i - 1].freq;
+    }
+}
+
+static void print_table() {
+    int summ = 0;
+    printf("[");
+    for (int i = 0; i < N; i++) {
+        summ += freqs[i].freq;
+        printf("%d, ", summ);
+    }
+    printf("]\n");
+}
 
 void compress_ppm(char *ifile, char *ofile) {
     FILE *ifp = (FILE *)fopen(ifile, "rb");
     FILE *ofp = (FILE *)fopen(ofile, "wb");
 
-    /** PUT YOUR CODE HERE
-      * implement a PPM algorithm for compression
-      * don't forget to change header file `ppm.h`
-    */
+    struct bit_rw_buf rbuf = {}, wbuf = {};
+    init_tables();
 
-    // This is an implementation of simple copying
-    size_t n, m;
-    unsigned char buff[8192];
+    fseek(ifp, 0, SEEK_END);
+    int32_t input_size = ftell(ifp);
+    fseek(ifp, 0, SEEK_SET);
 
-    do {
-        n = fread(buff, 1, sizeof buff, ifp);
-        if (n)
-            m = fwrite(buff, 1, n, ofp);
-        else 
-            m = 0;
-    } while ((n > 0) && (n == m));
+    fwrite(&input_size, sizeof(input_size), 1, ofp);
+
+    value_type left = 0, right = top_val;
+
+    int ch;
+    while ((ch = fgetc(ifp)) != -1) {
+        update_tables();
+        update_freqs_ranges();
+
+        unsigned long long delta = (unsigned long long)right - left + 1;
+        right = left + freq_ranges[ch + 1] * delta / freqs_summ - 1;
+        left = left + freq_ranges[ch] * delta / freqs_summ;
+#ifdef DEBUG_PRINT
+        printf("WRITE %d\n", ch);
+        printf("[%u %u]\n", left, right);
+#endif
+
+        while (1) {
+            if (right < half_val) {
+                output_bit(ofp, &wbuf, 0);
+            } else if (left >= half_val) {
+                left -= half_val;
+                right -= half_val;
+                output_bit(ofp, &wbuf, 1);
+            } else if (quart1_val <= left && right < quart3_val) {
+                left -= quart1_val;
+                right -= quart1_val;
+                prev_bits++;
+            } else 
+                break;
+            left <<= 1;
+            right <<= 1;
+            right++;
+            left &= top_val;
+            right &= top_val;
+#ifdef DEBUG_PRINT            
+            printf("[%u %u]\n", left, right);
+#endif
+        }
+
+        modify_table(ch);
+    }
+
+    // last interval position encoding
+    ++prev_bits;
+    if (left < quart1_val) {
+        output_bit(ofp, &wbuf, 0);
+    } else {
+        output_bit(ofp, &wbuf, 1);
+    }
+
+    write_buf(ofp, &wbuf);
 
     fclose(ifp);
     fclose(ofp);
@@ -32,23 +227,76 @@ void decompress_ppm(char *ifile, char *ofile) {
     FILE *ifp = (FILE *)fopen(ifile, "rb");
     FILE *ofp = (FILE *)fopen(ofile, "wb");
 
-    /** PUT YOUR CODE HERE
-      * implement a PPM algorithm for decompression
-      * don't forget to change header file `ppm.h`
-    */
+    struct bit_rw_buf rbuf = {}, wbuf = {};
+    init_tables();
 
-    // This is an implementation of simple copying
-    size_t n, m;
-    unsigned char buff[8192];
+    int32_t file_size;
+    fread(&file_size, sizeof(file_size), 1, ifp);
 
-    do {
-        n = fread(buff, 1, sizeof buff, ifp);
-        if (n)
-            m = fwrite(buff, 1, n, ofp);
-        else 
-            m = 0;
-    } while ((n > 0) && (n == m));
+    value_type value = 0;
+    for (int i = 0; i < BITS_USED; i++) {
+        value <<= 1;
+        value |= read_bit(ifp, &rbuf);
+    }
 
+    value_type left = 0, right = top_val;
+    
+    int wrote_bytes = 0;
+    for (int wrote_bytes = 0; wrote_bytes < file_size; wrote_bytes++) {
+        update_tables();
+        update_freqs_ranges();
+    
+        // printf("NEXT LEFT %d RIGHT %d\n", left, right);
+        long long cur_point = (((unsigned long long)value - left + 1) * (freqs_summ) - 1) / ((unsigned long long)right - left + 1);
+#ifdef DEBUG_PRINT
+        printf("CUR POINT %ld FREQS_SUMM %llu VALUE 0x%x %d\n", cur_point, freqs_summ, value, value);
+#endif
+        int i;
+        for (i = 1; i < N; i++) {
+            if (freq_ranges[i] > cur_point) {
+                break;
+            }
+        }
+        int symbol = freqs[i - 1].sym;
+        fputc(symbol, ofp);
+#ifdef DEBUG_PRINT
+        printf("WRITE %d\n", symbol);
+#endif
+
+        unsigned long long delta = (unsigned long long)right - left + 1;
+        right = left + freq_ranges[i] * delta / freqs_summ - 1;
+        left = left + freq_ranges[i - 1] * delta / freqs_summ;
+#ifdef DEBUG_PRINT
+        printf("[%u %u]\n", left, right);
+#endif
+
+        modify_table(symbol);
+
+        while (1) {
+            if (right < half_val) {
+            } else if (left >= half_val) {
+                value -= half_val;
+                right -= half_val;
+                left -= half_val;
+            } else if (quart1_val <= left && right < quart3_val) {
+                value -= quart1_val;
+                left -= quart1_val;
+                right -= quart1_val;
+            } else 
+                break;
+            value <<= 1;
+            value |= read_bit(ifp, &rbuf);
+            left <<= 1;
+            right <<= 1;
+            right++;
+            left &= top_val;
+            right &= top_val;
+            value &= top_val;
+#ifdef DEBUG_PRINT
+            printf("[%u %u]\n", left, right);
+#endif
+        }
+    }
     fclose(ifp);
     fclose(ofp);
 }
